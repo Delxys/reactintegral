33d82b5f5ef193ab9cb3e71a478968a7680789d9
Delxys <bausdelxys@gmail.com>
2021-12-23 11:23:30 +0300

    quiz from db v2

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -5,55 +5,64 @@ import CurrentQuestion from '../CurrentQuestion/CurrentQuestion'
 class Quiz extends Component {
   state = {
     activeQuestion: 0,
-    quiz: [
-      // {
-      //   questionNumber:1,
-      //   question:'В чем ограничение метода Симпсона?',
-      //   rightAnswerId:2,
-      //   answers: [
-      //     {text: 'Невозможность задания границ дробными чилами', id:1},
-      //     {text: 'Четное число разбиений', id:2},
-      //     {text: 'Невозможность изменять границы', id:3},
-      //     {text: 'Погрешность более 0.01', id:4}
-      //   ]
-      // },
-      // {
-      //   questionNumber:2,
-      //   question:'Неопределенный интеграл от функции - это...',
-      //   rightAnswerId:3,
-      //   answers: [
-      //     {text: 'площадь криволинейной трапеции, ограниченной графиком функции, осью абсцисс и еще двумя прямыми', id:1},
-      //     {text: 'одна первообразная функции', id:2},
-      //     {text: 'совокупность всех производных функции', id:3},
-      //     {text: 'промежуток, на котором необходимо проинтегрировать функцию', id:4}
-      //   ]
-      // },
-      // {
-      //   questionNumber:3,
-      //   question:'Чему равен неопределенный интеграл от 1?',
-      //   rightAnswerId:1,
-      //   answers: [
-      //     {text: 'x+C', id:1},
-      //     {text: '0', id:2},
-      //     {text: '1+С', id:3},
-      //     {text: 'С', id:4}
-      //   ]
-      // }
-    ]
+    rightAnswersCount: 0,
+        quiz:[{
+              answers: [
+              ],
+              correctanswer:0
+            }],
   }
 
-  onAnswerClickHandler=(answerId)=>{
-  console.log(answerId)
-  if(this.state.activeQuestion+1===this.state.quiz.length)
-    console.log('finished')
-    else{
-      this.setState({
-        activeQuestion: this.state.activeQuestion+1
-      })
+  onAnswerClickHandler=(answertext)=>{
+  console.log(answertext)
+  let cur = this.state.quiz[this.state.activeQuestion]
+  let ra = cur.answers[Number(cur.correctanswer)-1]
+  if(answertext===ra){
+    const timeout = window.setTimeout(() => {
+        if(this.isQuizFinished()){
+            this.setState({
+                rightAnswersCount:  this.state.rightAnswersCount + 1
+            });
+            alert('Првильных ответов: ' + String(this.state.rightAnswersCount)+' из '+String(this.state.quiz.length));
+            this.cleanState()
+            this.componentDidMount()
+        }else{
+            this.setState({
+                rightAnswersCount: this.state.rightAnswersCount + 1
+            });
+            this.setState({
+                activeQuestion: this.state.activeQuestion+1
+            })
+        }
+        window.clearTimeout(timeout);
+    }, 200)
+  }else{
+          if(this.isQuizFinished()){
+              alert('Првильных ответов: ' + String(this.state.rightAnswersCount)+' из '+String(this.state.quiz.length));   
+              this.cleanState()
+              this.componentDidMount()
+          }else{
+              this.setState({
+                  activeQuestion: this.state.activeQuestion+1
+              })
+          }
     }
- 
   }
-  getQuiz=()=>{
+  isQuizFinished(){
+    return this.state.activeQuestion + 1 === this.state.quiz.length
+}
+cleanState(){
+  this.setState({
+    activeQuestion: 0,
+    rightAnswersCount: 0,
+        quiz:[{
+              answers: [
+              ],
+              correctanswer:0
+            }]
+  })
+}
+  componentDidMount=()=>{
     const headers = {
       'Content-Type': 'application/json'
     }
@@ -69,26 +78,18 @@ class Quiz extends Component {
       .catch(error => console.log(error));
   }
   render() {
-    let test = this.state.quiz.map((question,index)=>{
-        return (<div>
-          <CurrentQuestion 
-              answers={this.state.quiz[this.state.activeQuestion].answers}
-              questiontext={this.state.quiz[this.state.activeQuestion].questiontext}
-              quizLenght = {this.state.quiz.length}
-              answerNumber = {this.state.activeQuestion+1 }
-              onAnswerClick={this.onAnswerClickHandler}
-            />
-          </div>);
-    });
     return ( 
       <div>
         <h1>Тест</h1>
-
-        <button onClick = {() =>this.getQuiz()}> Начать</button>
         <div className={'Quiz'}>
-          
           <div className={'QuizWrapper'}>
-          {test}
+          <CurrentQuestion 
+            answers={this.state.quiz[this.state.activeQuestion].answers}
+            questiontext={this.state.quiz[this.state.activeQuestion].questiontext}
+            quizLenght = {this.state.quiz.length}
+            answerNumber = {this.state.activeQuestion+1 }
+            onAnswerClick={this.onAnswerClickHandler}
+          />
           </div>
         </div>
       </div>
--- src/Components/Quiz/QuizAnswerList/QuizAnswerItem/QuizAnswerItem.js
+++ src/Components/Quiz/QuizAnswerList/QuizAnswerItem/QuizAnswerItem.js
@@ -3,9 +3,11 @@ const QuizAnswerItem = props => {
   return (
     <li
       className='QuizAnswerItem'
-      onClick={()=>props.onAnswerClick(props.answer.id)}
+      // onClick={()=>props.onAnswerClick(props.answer.id)}
+      onClick={()=>props.onAnswerClick(props.answer)}
     >
-      { props.answer.text }
+      {/* { props.answer.text } */}
+      { props.answer }
     </li>
   )
 }
